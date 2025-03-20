1c71d4248db9a3859f1a510ca57d3e5f7c10ec45
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-03-20 15:01:53 -0300

    :sparkles: feat: Adicionado AdministradorDTO

---
+++ Dominio/DTOs/AdministradorDTO.cs
@@ -0,0 +1,8 @@
+namespace MinimalAPI.Dominio.DTOs;
+
+public class AdministradorDTO
+{
+    public string Email { get; set; } = default!;
+    public string Senha { get; set; } = default!;
+    public string Perfil { get; set; } = default!;
+}
