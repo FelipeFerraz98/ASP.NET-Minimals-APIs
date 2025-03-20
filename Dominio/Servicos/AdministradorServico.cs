1db58c23411a295db724f504e254a03bcd573952
FelipeFerraz98 <felipeferraz1398@gmail.com>
2025-03-20 19:31:48 -0300

    :sparkles: feat: Adicionado endpoints Administradores

--- Dominio/Servicos/AdministradorServico.cs
+++ Dominio/Servicos/AdministradorServico.cs
@@ -17,4 +17,28 @@ public class AdministradorServico : IAdministradorServico
         var adm = _contexto.Administradores.Where(a => a.Email == loginDTO.Email && a.Senha == loginDTO.Password).FirstOrDefault();
         return adm;
     }
+    public Administrador Incluir(Administrador administrador)
+    {
+        _contexto.Administradores.Add(administrador);
+        _contexto.SaveChanges();
+        return administrador;
+    }
+
+    public List<Administrador> Todos(int? pagina)
+    {
+        var query =_contexto.Administradores.AsQueryable();
+        
+        int intensPorPagina = 10;
+
+        if(pagina != null)
+        {
+            query = query.Skip(((int)pagina - 1) * intensPorPagina).Take(intensPorPagina);
+        }
+        return query.ToList();
+    }
+
+    public Administrador? BuscaPorId(int id)
+    {
+        return _contexto.Administradores.Where(a => a.Id == id).FirstOrDefault();
+    }
 }
